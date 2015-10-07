e9d38e74f70a0e829836fd434e60c35d7c7b28ec
Jack Martin <JM273606@gmail.com>
2015-10-06 21:42:48 -0400

    stacked data struct

---
+++ stackDataStru.py
@@ -0,0 +1,20 @@
+stack = [66.25, 333, 333, 1, 1234.5]
+
+# Stack data Structure 
+
+# list = []
+
+# class stack(list):
+# 	def push(self, item):
+# 		self.append(item)
+# 	def isEmpty(self):
+# 		return not self
+
+print stack.append(2)
+print stack
+print stack.pop()
+
+
+def f(x): return x % 3 == 0 or x % 5 == 0 
+
+print filter(f, range(2, 25))
