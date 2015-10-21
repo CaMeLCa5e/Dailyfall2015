bc0bf3c06aa5538d56f35e5b7432cf518e77c997
Jack Martin <JM273606@gmail.com>
2015-10-20 22:38:37 -0400

    updates

---
+++ tomd.py
@@ -0,0 +1,10 @@
+md = [h1, h2, p]
+h1 = rawinput('please enter h1')
+h2 = rawinput('please enter h2')
+p = rawinput('please enter p')
+
+h1 = print '# ' + h1
+h2 = print '## ' + h2
+p = print p
+
+return md
