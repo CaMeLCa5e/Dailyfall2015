db4180a454f1bfb580a8b2a4fbf5fed645c18038
Jack Martin <JM273606@gmail.com>
2015-11-23 21:16:17 -0500

    updates

--- recursion.py
+++ recursion.py
@@ -14,3 +14,13 @@ def interactive_factorial (n):
     return result        
 
 print interactive_factorial(5)    
+
+def flatten_list(a, result=None):
+    if result is None; 
+        result = []
+    for x in a: 
+        if isinstance(x, list):
+            flatten_list(x, result)
+        else: 
+            result.append(x)
+    return result
