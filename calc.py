d251de735efa6852522789b0182c824133376eed
Jack Martin <JM273606@gmail.com>
2015-12-20 23:27:40 -0500

    updates

--- calc.py
+++ calc.py
@@ -1,8 +1,24 @@
-loop  = 1 
-choice = 0 
+# loop  = 1 
+# choice = 0 
 
-while loop == 1: 
-    print "welcome to calculator"
+# while loop == 1: 
+#     print "welcome to calculator"
+#     print "  "
+#     print "1, addition"
+#     print "2, subtraction"
+#     print "3, multiplication"
+#     print "4, division"
+#     print "5, quit"
+#     print "  "
+
+#     choice = input("choose your option")
+#     if choice == 1: 
+#         add1 = input("add this:")
+#         add2 = input("to this:")
+#         print add1, "+", add2, =, add1 + add2
+#         
+
+def menu():
     print "  "
     print "1, addition"
     print "2, subtraction"
@@ -10,10 +26,17 @@ while loop == 1:
     print "4, division"
     print "5, quit"
     print "  "
+    return input("choose an option: ")
 
-    choice = input("choose your option")
-    if choice == 1: 
-        add1 = input("add this:")
-        add2 = input("to this:")
-        print add1, "+", add2, =, add1 + add2
-        
+def add(a,b):
+    print a + b
+
+def sub(a,b):
+    print a - b
+
+def mul(a,b):
+    print a * b    
+
+def div(a,b):
+    print a / b
+    
